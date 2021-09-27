b5a978d3f3f13f300c268fb76e8dbf367fb3c2b6
Matthew Joughin <matthew@adaptableapps.net>
2021-09-27 08:24:22 +0200

    Working on this
    Signed-off-by: Matthew Joughin <matthew@adaptableapps.net>

---
+++ scripts/_install_net_6.0_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Running: sudo ./dotnet-core-uninstall remove --all-previews --sdk --force"
+sudo ./dotnet-core-uninstall remove --all-previews --sdk --force
+
+echo "Running: sudo ./dotnet-core-uninstall remove --all-previews --runtime --force"
+sudo ./dotnet-core-uninstall remove --all-previews --runtime --force
+
+echo "Running: "
+sudo dotnet workload uninstall wasm-tools
+
+echo "Running: sudo ./dotnet-install.sh -version 6.0.100-rc.1.21463.6 --install-dir /usr/local/share/dotnet"
+sudo ./dotnet-install.sh -version 6.0.100-rc.1.21463.6 --install-dir /usr/local/share/dotnet
+
+echo "Running: sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --sdk-version 6.0.100-rc.1.21463.6"
+sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --sdk-version 6.0.100-rc.1.21463.6
+
+echo "Done"
