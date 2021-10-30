52909d77d054525480d31869a086fb61335bbff7
Matthew Joughin <matthew@adaptableapps.net>
2021-10-30 13:22:22 +0200

    Working on this
    Signed-off-by: Matthew Joughin <matthew@adaptableapps.net>

---
+++ scripts/dotnet/_dotnet-install-workload-macos.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo dotnet workload install macos --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --skip-manifest-update --include-previews
