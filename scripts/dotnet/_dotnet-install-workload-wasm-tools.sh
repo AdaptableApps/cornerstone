10319d015462c086c1798922d319c05323606c98
Matthew Joughin <matthew@adaptableapps.net>
2021-11-01 18:02:37 +0200

    Working on this
    Signed-off-by: Matthew Joughin <matthew@adaptableapps.net>

--- scripts/dotnet/_dotnet-install-workload-wasm-tools.sh
+++ scripts/dotnet/_dotnet-install-workload-wasm-tools.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Running: sudo dotnet workload uninstall wasm-tools"
+sudo dotnet workload uninstall wasm-tools
+
+echo "Running: sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --skip-manifest-update --include-previews"
 sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --skip-manifest-update --include-previews
--- scripts/dotnet/_install_net_6.0_linux.sh
+++ scripts/dotnet/_install_net_6.0_linux.sh
@@ -8,13 +8,7 @@
 # echo "Running: sudo ./dotnet-core-uninstall remove --all-previews --runtime --force"
 # sudo ./dotnet-core-uninstall remove --all-previews --runtime --force
 
-echo "Running: "
-sudo dotnet workload uninstall wasm-tools
-
 echo "Running: sudo ./dotnet-install.sh -version 6.0.100-rc.2.21505.1 --install-dir /usr/local/share/dotnet"
 sudo ./dotnet-install.sh -version 6.0.100-rc.2.21505.1 --install-dir /usr/local/share/dotnet
-
-echo "Running: sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --sdk-version 6.0.100-rc.2.21505.1"
-sudo dotnet workload install wasm-tools --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --sdk-version 6.0.100-rc.2.21505.1
 
 echo "Done"
