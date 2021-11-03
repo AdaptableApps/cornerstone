bf79255059d309b8a415ded857f44c421e068de7
Matthew Joughin <matthew@adaptableapps.net>
2021-11-03 16:17:38 +0200

    Working on this
    Signed-off-by: Matthew Joughin <matthew@adaptableapps.net>

--- scripts/dotnet/_dotnet-install-workload-macos.sh
+++ scripts/dotnet/_dotnet-install-workload-macos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Running: sudo dotnet workload uninstall macos"
-sudo dotnet workload uninstall macos
+# echo "Running: sudo dotnet workload uninstall macos"
+# sudo dotnet workload uninstall macos
 
 sudo dotnet workload install macos --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json --skip-manifest-update --include-previews
